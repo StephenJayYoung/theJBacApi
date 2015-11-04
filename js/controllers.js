07e28de7ac99f98424b4466bc9ed38bd6ba8bb08
Stephen Young <stephenjayyoung@gmail.com>
2015-11-03 16:10:56 -0800

    working on getting data to appear

--- js/controllers.js
+++ js/controllers.js
@@ -5,4 +5,14 @@ var controllers = angular.module('stravaApi.controllers',[]);
 controllers.controller('AppCtrl', function ($scope) {
 	$scope.name = "Module";
 
+});
+
+controllers.controller('AthletesController', function($scope, $http) {
+	$scope.list
+
+	$http.jsonp('https://www.strava.com/api/v3/oauth').then(function (data) {
+		console.log(data);
+
+	})
+
 })
