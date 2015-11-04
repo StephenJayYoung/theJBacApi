3b9677fc441ea5b827ba04172ca50984e0f14f0a
Stephen Young <stephenjayyoung@gmail.com>
2015-11-03 16:23:33 -0800

    trying to show 201 by using dummy client secret and dummy client id

--- js/controllers.js
+++ js/controllers.js
@@ -9,8 +9,9 @@ controllers.controller('AppCtrl', function ($scope) {
 
 controllers.controller('AthletesController', function($scope, $http) {
 	$scope.list
+	var access= 'https://www.strava.com/api/v3/athlete + client_id=9 + client_secret=7b2946535949ae70f015d696d8ac602830ece412 + client_code=75e251e3ff8fff'
 
-	$http.jsonp('https://www.strava.com/api/v3/oauth').then(function (data) {
+	$http.jsonp('https://www.strava.com/api/v3/athlete').then(function (data) {
 		console.log(data);
 
 	})
